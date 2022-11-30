468deb66103a1e8888675575d4bc5bf8f5da7888
Johnit Jasan <jjohnit@gmail.com>
2022-11-30 17:53:03 -0600

    Working till restarting foreground service

--- app/src/main/java/com/jjasan2/clipserver/ClipServerService.java
+++ app/src/main/java/com/jjasan2/clipserver/ClipServerService.java
@@ -1,5 +1,8 @@
 package com.jjasan2.clipserver;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
@@ -7,11 +10,19 @@ import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
 
+import androidx.core.app.NotificationCompat;
+
 public class ClipServerService extends Service {
 
     protected final String TAG = "appDebugService";
-    private int mStartID;
 
+    // Id to recognise the instance of service (for stopping)
+    private int mStartID;
+    Notification notification;
+    // Unique id for the notification
+    private static final String CHANNEL_ID = "Clip server notification" ;
+
+    // The list of music files
     protected int[] musicList = new int[] {
             R.raw.a_very_happy_christmas, R.raw.dance_with_me, R.raw.delightful,
             R.raw.hollidays, R.raw.sleepy_cat, R.raw.vintage_telephone_ringtone
@@ -22,13 +33,19 @@ public class ClipServerService extends Service {
     private final ClipServerServices.Stub mBinder = new ClipServerServices.Stub() {
 
         @Override
+        public boolean startService() {
+            startForeground(1, notification);
+            return true;
+        }
+
+        @Override
         public boolean play(int songIndex) {
-            Log.i(TAG, "songIndex is" + songIndex);
-            if(songIndex > 0 && songIndex < musicList.length){
-                // Stop playback is another song is already playing
-                if (null != mPlayer) {
-                    mPlayer.stop();
-                }
+            Log.i(TAG, "songIndex is " + songIndex);
+            if(songIndex > 0 && songIndex <= musicList.length){
+                // Stop playback if another song is already playing
+//                if (mPlayer != null) {
+//                    mPlayer.stop();
+//                }
 
                 mPlayer = MediaPlayer.create(getBaseContext(), musicList[songIndex - 1]);
                 mPlayer.start();
@@ -63,11 +80,31 @@ public class ClipServerService extends Service {
             }
             return false;
         }
+
+        @Override
+        public boolean stopService() {
+            if(mPlayer != null){
+                mPlayer.stop();
+            }
+
+            stopSelf(mStartID);
+            return true;
+        }
     };
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // For creating the notification for the foreground service
+        this.createNotificationChannel();
+
+        notification = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.ic_media_play)
+                .setOngoing(true).setContentTitle("Clip server running")
+                .build();
+
+        startForeground(1, notification);
 
         if(mPlayer != null){
             // Stop Service when music has finished playing
@@ -76,10 +113,8 @@ public class ClipServerService extends Service {
                 @Override
                 public void onCompletion(MediaPlayer mp) {
 
-                    // stop Service if it was started with this ID
-                    // Otherwise let other start commands proceed
+                    // un bind from the client when the playback stops
                     stopSelf(mStartID);
-
                 }
             });
         }
@@ -87,8 +122,25 @@ public class ClipServerService extends Service {
         Log.i(TAG, "Service created");
     }
 
+    // For creating the notification channel for the foreground service
+    private void createNotificationChannel() {
+        CharSequence name = "Clip server notification";
+        String description = "The channel for clip server notifications";
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel ;
+            channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "onStartCommand: ");
         super.onStartCommand(intent, flags, startId);
         mStartID = startId;
         return START_NOT_STICKY;
@@ -96,17 +148,18 @@ public class ClipServerService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
+        Log.i(TAG, "onBind: ");
         return mBinder;
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
+        Log.i(TAG, "onUnbind: ");
         super.onUnbind(intent);
 
         if (null != mPlayer) {
 
             mPlayer.stop();
-            mPlayer.release();
 
         }
         return true;
@@ -114,6 +167,7 @@ public class ClipServerService extends Service {
 
     @Override
     public void onDestroy() {
+        Log.i(TAG, "onDestroy: ");
         super.onDestroy();
 
         if (null != mPlayer) {
--- app/src/main/java/com/jjasan2/clipserver/MainActivity.java
+++ app/src/main/java/com/jjasan2/clipserver/MainActivity.java
@@ -1,8 +1,12 @@
 package com.jjasan2.clipserver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
+
+        // Start the foreground service
+        Intent intent =new Intent(getApplicationContext(), ClipServerService.class);
+        startService(intent);
+    }
+
+    public void onStart() {
+        super.onStart();
+
+        // Request permission for foreground service
+        if (ActivityCompat.checkSelfPermission(
+                this,"android.permission.FOREGROUND_SERVICE")
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[] {"android.permission.FOREGROUND_SERVICE"}, 1) ;
+        }
+
+        // Request permission for showing notification
+        if (ActivityCompat.checkSelfPermission(
+                this,"android.permission.POST_NOTIFICATIONS")
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[] {"android.permission.POST_NOTIFICATIONS"}, 0) ;
+        }
+    }
+
+    public void onRequestPermissionsResult(int code, String[] permissions, int[] result) {
+        super.onRequestPermissionsResult(code, permissions, result) ;
+        if (result.length >0) {
+            if (result[0] == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(this, "Missing permissions to run foreground service", Toast.LENGTH_SHORT).show() ;
+            }
+        }
     }
 }
